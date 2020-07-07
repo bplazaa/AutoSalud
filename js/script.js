e776c30d4e261f773224238adbb7ee7344848786
Bryan Plaza <bryanplaza_@hotmail.com>
2020-07-07 16:03:37 -0500

    Cambio en disenio de Equipo

--- js/script.js
+++ js/script.js
@@ -67,37 +67,5 @@ $(function() {
             }
         }
     });
-    /*-----------------------------------
-     * OWL CAROUSEL
-     *-----------------------------------*/
-    var $testimonialsDiv = $('.team');
-    if ($testimonialsDiv.length && $.fn.owlCarousel) {
-        $testimonialsDiv.owlCarousel({
-            items: 1,
-            nav: true,
-            dots: false,
-            navText: ['<span class="ti-arrow-left"></span>', '<span class="ti-arrow-right"></span>']
-        });
-    }
-
-    var $galleryDiv = $('.img-gallery');
-    if ($galleryDiv.length && $.fn.owlCarousel) {
-        $galleryDiv.owlCarousel({
-            nav: false,
-            center: true,
-            loop: true,
-            autoplay: true,
-            dots: true,
-            navText: ['<span class="ti-arrow-left"></span>', '<span class="ti-arrow-right"></span>'],
-            responsive: {
-                0: {
-                    items: 1
-                },
-                768: {
-                    items: 3
-                }
-            }
-        });
-    }
-
+ 
 }); /* End Fn */
